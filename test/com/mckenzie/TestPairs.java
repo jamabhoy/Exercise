63c8fc73a19bb3276409bb9436721c54c9d2ffbd
jamabhoy <james@reilly-family.co.uk>
2019-08-21 12:17:22 +0100

    Initial commit

--- test/com/mckenzie/TestPairs.java
+++ test/com/mckenzie/TestPairs.java
@@ -7,10 +7,8 @@ public class TestPairs {
     @Test
     public void test1()
     {
-        Pairs myPairs = new Pairs();
-
-        // expected (in any order, not exhaustive): (0,8),(1,7),...,(9,-1)
-        int[] numbers = {8, 0, 1, 7, 2, 4, 3, 3, 6, 5, 9, -1};
-        System.out.println(myPairs.findArrayPairSums(numbers, 8));
+        /* expected (in any order, not exhaustive): (0,8),(1,7),...,(9,-1) */
+        int[] numbers = {8, 0, 1, 7, 2, 4, 3, 3, 6, 5, 9, -1, 4, 5};
+        System.out.println(Pairs.findArrayPairSums(numbers, 8));
     }
 }
